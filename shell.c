b6d33dfb27109963568eee37084c0075e6999b3e
yumingkuan <kkhimself@foxmail.com>
2024-02-02 16:11:46 +0000

    loop and getline

---
+++ shell.c
@@ -0,0 +1,74 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+
+char *read_line(void) {
+    char *line = NULL;
+    size_t bufsize = 0;
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return line;
+}
+
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+char **split_line(char *line) {
+    int bufsize = TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+void shell_loop(void) {
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf(">");
+        line = read_line();
+        args = split_line(line);
+        status = exec(args);
+        free(line);
+        free(args);
+    } while (status);
+}
+
+
+
+
+int main(int argc, char **argv) {
+    printf("asd");
+    shell_loop();
+    return EXIT_SUCCESS;
+}
+
